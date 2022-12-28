b64c6b6b958da938c1b630b889fd65c12ffbdf88
Klambotsky Konstantin <kolobchara@gmail.com>
2022-12-28 20:31:24 +0700

    Simple AI

--- Assets/Scripts/Movement/AIControlled.cs
+++ Assets/Scripts/Movement/AIControlled.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 
 public class AIControlled : MovementBehaviour
 {
-    private void Start()
+    [SerializeField]
+    private LayerMask gunnerMask;
+
+    [SerializeField] Gunner gunner;
+
+    private float timer = 0;
+    private float cooldown = 0.1f;
+
+    private void Update()
     {
-        StartCoroutine(AI());
-    }
-    private IEnumerator AI()
-    {
-        while (true)
+        timer += Time.deltaTime;
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10, gunnerMask))
         {
-            direction = transform.forward * Random.Range(-1, 1) * moveSpeed;
-            rotation.y = Random.Range(-1, 1) * rotateSpeed;
-            yield return new WaitForSeconds(3);
+            if (timer >= cooldown)
+            {
+                gunner.Shoot();
+                timer = 0;
+            }
         }
+        rotation.y = Random.Range(-1, 1) * rotateSpeed;
     }
 }
